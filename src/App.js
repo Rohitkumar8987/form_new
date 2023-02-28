b74a75851491a251f0a0f73b210921a22d868845
Rohit kumar <122736775+Rohitkumar8987@users.noreply.github.com>
2023-02-28 10:01:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// import Customer from './component/Customer';
+// import NewClient from './component/Customer/NewClient';
+// import Purchaseorder from './component/Customer/Purchaseorder';
+// import customer from './component/Customer/Customer';
+import Customer from './component/Customer/Customer';
+
+const App=()=> {
+  return (
+    <>
+    <Customer/>
+  
+    </>
+
+
+  );
+};
+
+export default App;
+
---
+++ src/component/Customer/Purchaseorder.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import "./Purchaseorder.css";
+
+
+export default function Purchaseorder() {
+    const handleHover = () => {
+
+        // return "hello"
+        console.log("hover")
+    }
+  return (
+    <div className="container">
+    <form action="">
+    <h1>Purchase Order Details</h1>
+    <div className="content">
+        <div className="input-box">
+          <label for="name">Created by</label>
+          <input type="text" placeholder="" />
+        </div>
+        <div className="input-box">
+          <label for="name">Created date</label>
+          <input type="text" placeholder="" />
+        </div>
+        <div className="input-box">
+          <label for="name">PO#</label>
+          <input type="text" placeholder="" />
+        </div>
+        <div className="input-box_o">
+          <label for="name">Vendor</label>
+          <div className='select'>
+          <select name="" id="">
+          <option value="">1 Vendor Name here <span className='red' style={{ color : 'red'}}>  Min Order $500.00</span></option>
+        </select>
+        </div>
+        </div>
+          <div className='items' style={{display: "flex", justifyContent: 'space-between'}}>
+           <div>Items</div>
+           <div>Total Price: $0.00</div>
+          </div>
+          
+
+          <div className="input-box_op">
+          
+          <div className="input-box_p">
+          <label for="name">item</label>
+          <select name="" id="">
+          <option value=""></option>
+        </select>
+        </div>
+          <div className="input-box-po">
+          <label for="name">QTY</label>
+          <select name="" id="">
+          <option value=""></option>
+        </select>
+        </div>
+        <div className="input-box_l">
+          <label for="name">Price</label>
+          <input type="text" placeholder="" />
+        </div>
+        </div>
+        
+        </div>
+        <div className='more_btn'>
+            <span></span>
+        </div>
+
+{/* <div className="line1">
+  <table border="1">
+    <tr>
+      <td>
+        <span id="a">this is sick</span><span id="b">this is awesome</span>
+      </td>
+    </tr>
+  </table>
+</div> */}
+        <div className="button-container">
+      <div className="btn">
+        <button className="save" type="submit">Save</button>
+        <div className="btn-o">
+        <button type="clear:  ">Cancel</button>
+        </div>
+        </div>
+      </div>
+    </form>
+    </div>
+  )
+}
